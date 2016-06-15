8ef79adf819e0ad67ea97485985c386671cc2ae0
Daniel Borkmann <daniel@iogearbox.net>
2016-06-15 22:47:12 +0200

    bpf, maps: add release callback
    Add a release callback for maps that is invoked when the last
    reference to its struct file is gone and the struct file about
    to be released by vfs. The handler will be used by fd array maps.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -143,7 +143,12 @@ static void bpf_map_show_fdinfo(struct seq_file *m, struct file *filp)
 
 static int bpf_map_release(struct inode *inode, struct file *filp)
 {
-	bpf_map_put_with_uref(filp->private_data);
+	struct bpf_map *map = filp->private_data;
+
+	if (map->ops->map_release)
+		map->ops->map_release(map, filp);
+
+	bpf_map_put_with_uref(map);
 	return 0;
 }
 
