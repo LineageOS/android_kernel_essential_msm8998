61e8051efcf52a6968fd06b389e9b5ca415e5352
Dan Pasanen <dan.pasanen@gmail.com>
2018-03-01 12:31:04 -0600

    Revert "arm64: SW PAN: Point saved ttbr0 at the zero page when switching to init_mm"
    This reverts commit 036efd57da6c5b877668d7b2c1671e321554942f.
    
    Change-Id: I3a788b147baed84bb167d69d3230c9f79c2c3c68

--- arch/arm64/include/asm/mmu_context.h
+++ arch/arm64/include/asm/mmu_context.h
@@ -176,17 +176,11 @@ void check_and_switch_context(struct mm_struct *mm, unsigned int cpu);
 static inline void update_saved_ttbr0(struct task_struct *tsk,
 				      struct mm_struct *mm)
 {
-	u64 ttbr;
-
-	if (!system_uses_ttbr0_pan())
-		return;
-
-	if (mm == &init_mm)
-		ttbr = __pa_symbol(empty_zero_page);
-	else
-		ttbr = virt_to_phys(mm->pgd) | ASID(mm) << 48;
-
-	task_thread_info(tsk)->ttbr0 = ttbr;
+	if (system_uses_ttbr0_pan()) {
+		BUG_ON(mm->pgd == swapper_pg_dir);
+		task_thread_info(tsk)->ttbr0 =
+			virt_to_phys(mm->pgd) | ASID(mm) << 48;
+	}
 }
 #else
 static inline void update_saved_ttbr0(struct task_struct *tsk,
@@ -232,9 +226,11 @@ switch_mm(struct mm_struct *prev, struct mm_struct *next,
 	 * Update the saved TTBR0_EL1 of the scheduled-in task as the previous
 	 * value may have not been initialised yet (activate_mm caller) or the
 	 * ASID has changed since the last run (following the context switch
-	 * of another thread of the same process).
+	 * of another thread of the same process). Avoid setting the reserved
+	 * TTBR0_EL1 to swapper_pg_dir (init_mm; e.g. via idle_task_exit).
 	 */
-	update_saved_ttbr0(tsk, next);
+	if (next != &init_mm)
+		update_saved_ttbr0(tsk, next);
 }
 
 #define deactivate_mm(tsk,mm)	do { } while (0)
