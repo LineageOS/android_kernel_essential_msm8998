4245e3e1ad4ced59d513a1dc4ec4c7d2dad62614
laserbeamh <32146457+laserbeamh@users.noreply.github.com>
2020-04-25 14:22:40 -0400

    remove two module references

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -4309,7 +4309,6 @@ static int ffs_init(void)
 
 	return 0;
 }
-module_init(ffs_init);
 
 static void __exit ffs_exit(void)
 {
@@ -4332,7 +4331,6 @@ static void __exit ffs_exit(void)
 		ffs_ipc_log = NULL;
 	}
 }
-module_exit(ffs_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Michal Nazarewicz");
