a0b11b421386defb05b8319b1418079018db0c35
Nolen Johnson <johnsonnolen@gmail.com>
2022-04-17 18:26:45 -0400

    [DNM] Squashed backport of BPF
    Change-Id: Ia061e4c1416f7cad44e982dfd43026092b0d2745

--- net/netfilter/xt_quota2.c
+++ net/netfilter/xt_quota2.c
@@ -346,6 +346,7 @@ static struct xt_match quota_mt2_reg[] __read_mostly = {
 		.match      = quota_mt2,
 		.destroy    = quota_mt2_destroy,
 		.matchsize  = sizeof(struct xt_quota_mtinfo2),
+		.usersize   = offsetof(struct xt_quota_mtinfo2, master),
 		.me         = THIS_MODULE,
 	},
 	{
@@ -356,6 +357,7 @@ static struct xt_match quota_mt2_reg[] __read_mostly = {
 		.match      = quota_mt2,
 		.destroy    = quota_mt2_destroy,
 		.matchsize  = sizeof(struct xt_quota_mtinfo2),
+		.usersize   = offsetof(struct xt_quota_mtinfo2, master),
 		.me         = THIS_MODULE,
 	},
 };
